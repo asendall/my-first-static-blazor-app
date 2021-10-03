a42aaacb6dc83c762bb6190f77a759d2dca28e66
asendall <andrew.sendall@gmail.com>
2021-10-03 12:58:32 +0100

    Sanity check!

--- Api/GetAuthTokensAsyncFunction.cs
+++ Api/GetAuthTokensAsyncFunction.cs
@@ -23,22 +23,13 @@ namespace BlazorApp.Api
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            try
-            {
-                string code = req.Query["code"];
-                var client = new OAuth2Client(_options.ClientId, _options.ClientSecret, _options.RedirectUrl, _options.Environment);
 
-                var tokenResponse = client.GetBearerTokenAsync(code).Result;
+            //string code = req.Query["code"];
+            //var client = new OAuth2Client(_options.ClientId, _options.ClientSecret, _options.RedirectUrl, _options.Environment);
 
-                return new OkObjectResult(tokenResponse.AccessToken);
-            }
-            catch (System.Exception ex)
-            {
+            //var tokenResponse = client.GetBearerTokenAsync(code).Result;
 
-                return new OkObjectResult(ex.Message); ;
-            }
-
-
+            return new OkObjectResult("It's not me");
             
         }
     }
