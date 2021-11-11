e52820518adc25968fef54bfa6cd11b9d347369c
asendall <andrew.sendall@gmail.com>
2021-11-11 20:41:49 +0000

    Added Shared Packages

--- Client/Program.cs
+++ Client/Program.cs
@@ -18,7 +18,8 @@ namespace BlazorApp.Client
             builder.RootComponents.Add<App>("app");
 
             var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
-                builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) })
+            
+            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) })
                     .AddStaticWebAppsAuthentication();
 
 
